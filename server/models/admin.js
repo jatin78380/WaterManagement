5c056e4282557ae4d95b1cdbfd59c11e3ba2cf47
jatinssh <jatin7838066@gmail.com>
2024-06-18 15:16:12 +0530

    updated

--- server/models/admin.js
+++ server/models/admin.js
@@ -1,5 +1,5 @@
 const  mongoose = require ('mongoose');
-mongoose.connect('mongodb+srv://jatin8612:Jatin%40rkblbrrk!12@cluster0.nkvwqm9.mongodb.net/')
+mongoose.connect('mymongourl')
 const AdminSchema = new mongoose.Schema({
 email: String,
 password: String,
